542ade76e53db70810d9cfb9fc56c60f49fc784f
Valery Yatsynovich <valery_yatsynovich@epam.com>
2026-06-12 15:49:54 +0300

    Migrate from javax.annotation to JSpecify annotations

--- crawler4j-core/src/main/java/edu/uci/ics/crawler4j/parser/css/ThrowingCSSParseExceptionCallback.java
+++ crawler4j-core/src/main/java/edu/uci/ics/crawler4j/parser/css/ThrowingCSSParseExceptionCallback.java
@@ -19,7 +19,7 @@
  */
 package edu.uci.ics.crawler4j.parser.css;
 
-import javax.annotation.Nonnull;
+import org.jspecify.annotations.NonNull;
 
 import com.helger.css.handler.ICSSParseExceptionCallback;
 import com.helger.css.parser.ParseException;
@@ -27,7 +27,7 @@ import com.helger.css.parser.ParseException;
 public class ThrowingCSSParseExceptionCallback implements ICSSParseExceptionCallback {
 	
 	@Override
-	public void onException (@Nonnull final ParseException ex) {
+	public void onException (@NonNull final ParseException ex) {
 		throw new RuntimeException(ex);
 	}
 }
